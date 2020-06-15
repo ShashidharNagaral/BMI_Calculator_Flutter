9388534ca508f695178996895fd40dae39d0ba52
ShashidharNagaral <shashinagaral123@gmail.com>
2020-06-16 00:37:52 +0530

    added screenshots

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class BMICalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: InputPage(),
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0E21),
         textTheme: TextTheme(
